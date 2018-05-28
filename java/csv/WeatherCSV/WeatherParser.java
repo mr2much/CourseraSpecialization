5671eca7ed4c4d08fa21712d703f3bd227a8224c
Anubis Lockward <anubis.lockward@gmail.com>
2018-05-28 07:01:47 -0400

    Implemented getAverageTemp and getMaxTemp

--- java/csv/WeatherCSV/WeatherParser.java
+++ java/csv/WeatherCSV/WeatherParser.java
@@ -11,10 +11,15 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import java.util.List;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class WeatherParser {
     public FileResource getEmptyFileResource() {
         return new FileResource();
+    }
+    
+    public FileResource getFileResourceFor(String filename) {
+        return new FileResource(filename);
     }
     
     public CSVParser getCSVParser(FileResource resource) {
@@ -36,6 +41,11 @@ public class WeatherParser {
         // TODO: test if record count is lower than average for a given file
         
         testRecordCount(parser.getRecords());
+        testAverages();
+        
+        // TODO: change getMaxTemp method to return CSVRecord with
+        // highest temperature, instead of returning a float
+        testMaxTemp();
         
         System.out.println("Tests finished.");
     }
@@ -66,5 +76,85 @@ public class WeatherParser {
             System.out.println("Possible error, number of records is " +
                 records.size());
         }
+        
+        System.out.println("Finished counting records.");
+    }
+    
+    public void testAverages() {
+        FileResource fr = getFileResourceFor(
+            Paths.get("nc_weather/2012/weather-2012-01-01.csv").toString());
+        float value = getAverageTemp(fr.getCSVParser());
+        
+        if (value < 52.61) {
+            System.out.println("Expected 52.61 but got " + value);
+        }
+        
+        value = getAverageTemp(null);
+        
+        if (value != 0.0f) {
+            System.out.println("Value should be 0.0, got " + value);
+        }
+        
+        System.out.println("Finished testing average temperature.");
+    }
+    
+    public float getAverageTemp(CSVParser parser) {
+        
+        if (parser == null) {
+            return 0.0f;
+        }
+        
+        int recordCount = 0;
+        float tempTotal = 0.0f;
+        
+        for (CSVRecord record : parser) {
+            String temp = record.get("TemperatureF");
+            
+            float t = Float.parseFloat(temp);
+            
+            tempTotal += t;
+            
+            recordCount++;
+        }
+        
+        return tempTotal / recordCount;
+    }
+    
+    public void testMaxTemp() {
+        FileResource fr = getFileResourceFor(
+            Paths.get("nc_weather/2012/weather-2012-01-01.csv").toString());
+        float value = getMaxTemp(fr.getCSVParser());
+        
+        if (value < 66.9) {
+            System.out.println("Expected 66.9 but got " + value);
+        }
+        
+        value = getMaxTemp(null);
+        
+        if (value != 0.0f) {
+            System.out.println("Expected 0.0f got " + value);
+        }
+        
+        System.out.println("Finished testing max temperature");
+    }
+    
+    public float getMaxTemp(CSVParser parser) {
+        
+        if (parser == null) {
+            return 0.0f;
+        }
+        
+        float max = 0.0f;
+        
+        for (CSVRecord record : parser) {
+            String tempStr = record.get("TemperatureF");
+            float currentTemp = Float.parseFloat(tempStr);
+            
+            if (currentTemp > max) {
+                max = currentTemp;
+            }
+        }
+        
+        return max;
     }
 }
