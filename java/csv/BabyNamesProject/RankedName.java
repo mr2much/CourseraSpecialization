1e4ec7256c94da6c1cd3bbb2084a81c4e2ab3fa4
Anubis Lockward <anubis.lockward@gmail.com>
2018-06-13 23:00:13 -0400

    Implemented compareTo for RankedNames

--- java/csv/BabyNamesProject/RankedName.java
+++ java/csv/BabyNamesProject/RankedName.java
@@ -5,18 +5,18 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class RankedName {
+public class RankedName implements Comparable<RankedName> {
     public String name;
     public String sex;
-    public int count;
+    public Integer count;
     
     public String getName() { return name; }
     
     public String getSex() { return sex; }
     
-    public int getCount() { return count; }
+    public Integer getCount() { return count; }
     
-    public RankedName(String name, String sex, int count) {
+    public RankedName(String name, String sex, Integer count) {
         this.name = name;
         this.sex = sex;
         this.count = count;
@@ -24,6 +24,8 @@ public class RankedName {
     
     @Override
     public boolean equals(Object o) {
+        if (!(o instanceof RankedName)) { return false; }
+        
         RankedName other = (RankedName) o;
         
         if (this.count == other.getCount() && this.sex.equals(other.getSex())) {
@@ -31,5 +33,15 @@ public class RankedName {
         }
         
         return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+    
+    @Override
+    public int compareTo(RankedName otherName) {
+        return this.count.compareTo(otherName.getCount());
     }
 }
--- java/csv/BabyNamesProject/tester.java
+++ java/csv/BabyNamesProject/tester.java
@@ -11,6 +11,8 @@ public class tester {
         testSameRankedNameSameSexEquality();
         testSameRankedNameDifferentSexEquality();
         testDifferentRankedNameSameSexEquality();
+        testCompareToSameSex();
+        // testCompareToDifferentSex();
         //testRankOfNameToday();
         
         System.out.println("Test finished.");
@@ -20,6 +22,10 @@ public class tester {
         RankedName jennifer = new RankedName("Jennifer", "F", 21);
         assert jennifer.equals(new RankedName("Grace", "F", 21)) :
             "Jennifer's rank should be equal to Grace's";
+        assert jennifer.equals(new RankedName("Erica", "F", 21));
+        assert jennifer.equals(new RankedName("Barbara", "F", 21));
+        assert jennifer.equals(new RankedName("Crystal", "F", 21));
+        assert jennifer.equals(new RankedName("Alexandra", "F", 21));
     }
     
     public void testSameRankedNameDifferentSexEquality() {
@@ -32,6 +38,31 @@ public class tester {
         RankedName jennifer = new RankedName("Jennifer", "F", 21);
         assert !jennifer.equals(new RankedName("Jule", "F", 13)) :
             "Names should not be equal if ranks are different";
+    }
+
+    public void testCompareToSameSex() {
+       RankedName jennifer = new RankedName("Jennifer", "F", 21);
+       RankedName barbara = new RankedName("Barbara", "F", 13);
+       RankedName crystal = new RankedName("Crystal", "F", 22);
+       RankedName anubis = new RankedName("Anubis", "M", 99);
+       RankedName erica = new RankedName("Erica", "F", 21);
+       
+       assert jennifer.compareTo(barbara) > 0 : 
+        "Jennifer ranks higher than Barbara";
+       assert jennifer.compareTo(crystal) < 0 :
+        "Jennifer ranks lower than Crystal";
+       assert jennifer.compareTo(erica) == 0 :
+        "Jennifer ranks equal to Erica";
+    }
+    
+    public void testCompareToDifferentSex() {
+        RankedName jennifer = new RankedName("Jennifer", "F", 21);
+        RankedName anubis = new RankedName("Anubis", "M", Integer.MAX_VALUE);
+        
+        assert jennifer.compareTo(anubis) > 0 :
+            "Female names have to rank higher than Male names";
+        assert anubis.compareTo(jennifer) < 0 :
+            "Male names have to rank lower than Female names";
     }
     
     // public void testRankOfNameToday() {
