be30f01ab28422c37bd3ceae90469716bd4f13c8
Anubis Lockward <anubis.lockward@gmail.com>
2018-06-22 07:03:28 -0400

    Implemented method for reading all lines in file

--- java/csv/BabyNamesProject/src/FileManager.java
+++ java/csv/BabyNamesProject/src/FileManager.java
@@ -5,9 +5,27 @@ package src;
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.ArrayList;
+
 public class FileManager {
     public static String RESOURCE_PATH = "res";
     public static String DECADE_PATH = "us_babynames_by_decade";
     public static String YEAR_PATH = "us_babynames_by_year";
+ 
+    public List<String> readAllLines(Path path) throws IOException {
+        if (pathExists(path)) {
+            return Files.readAllLines(path, StandardCharsets.UTF_8);
+        }
+        
+        return new ArrayList<String>();
+    }
     
+    public boolean pathExists(Path path) {
+        return Files.exists(path);
+    }
 }
--- java/csv/BabyNamesProject/tests/FilesTester.java
+++ java/csv/BabyNamesProject/tests/FilesTester.java
@@ -8,9 +8,58 @@ package tests;
  * @version (a version number or a date)
  */
 import src.FileManager;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class FilesTester {
-    public static void main(String[] args) {
-        performTest();
+    public void performTests() {
+        shouldFailIfPathDoesntExist();
+        fileShouldHaveTenLines();
+        shouldThrowErrorIfLinesAreNotEqual();
+        System.out.println("Tests finished.");
+    }
+    
+    public void shouldFailIfPathDoesntExist() {
+        FileManager fm = new FileManager();
+        
+        assert fm.pathExists(Paths.get("res"));
+        assert fm.pathExists(Paths.get("res", "example-small.csv"));
+    }
+    
+    public void fileShouldHaveTenLines() {
+        FileManager fm = new FileManager();
+        
+        try {
+            List<String> lines = fm.readAllLines(
+                Paths.get("res", "example-small.csv"));
+                
+            assert lines.size() == 10;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void shouldThrowErrorIfLinesAreNotEqual() {
+        FileManager fm = new FileManager();
+        
+        try {
+            List<String> lines = fm.readAllLines(
+                Paths.get("res", "example-small.csv"));
+            
+            compare(lines, new String[] {"Emma,F,500", "Olivia,F,400", 
+                "Sophia,F,300", "Isabella,F,200", "Ava,F,100", "Noah,M,100", 
+                "Liam,M,40", "Mason,M,30", "Jacob,M,20", "William,M,10"});
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private void compare(List<String> namesList, String[] namesArr) {
+        Object[] arr = namesList.toArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            assert arr[i].equals(namesArr[i]);
+        }
     }
 }
