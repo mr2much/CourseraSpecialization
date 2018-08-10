0cb6e0bc5d9056c9875bc168c24c4b70d3808a0a
Anubis Lockward <anubis.lockward@gmail.com>
2018-08-10 06:55:01 -0400

    Added code to test encryption on CaesarCipher

--- java/strings/VigenereProgram/CaesarCipherTester.java
+++ java/strings/VigenereProgram/CaesarCipherTester.java
@@ -5,6 +5,22 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CaesarCipherTester {
 
+import edu.duke.FileResource;
+
+public class CaesarCipherTester {
+    private FileResource fr = new FileResource("messages/titus-small.txt");
+    
+    public void performTests() {
+        testEncryption();
+        System.out.println("OK");
+    }
+    
+    public void testEncryption() {
+        CaesarCipher cc = new CaesarCipher(17);
+        
+        String encrypted = cc.encrypt(fr.asString());
+        
+        System.out.println(encrypted);
+    }
 }
