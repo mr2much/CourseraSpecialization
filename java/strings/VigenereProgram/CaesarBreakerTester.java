edcc4fd6b0fa03cc5d7de7abb30ed4419b078fa0
Anubis Lockward <anubis.lockward@gmail.com>
2018-08-13 07:20:08 -0400

    Added tests for CaesarBreaker and VigenereCipher

--- java/strings/VigenereProgram/CaesarBreakerTester.java
+++ java/strings/VigenereProgram/CaesarBreakerTester.java
@@ -5,6 +5,32 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CaesarBreakerTester {
 
+import edu.duke.FileResource;
+
+public class CaesarBreakerTester {
+    private FileResource fr;
+    public void performTest() {
+        testSmallKey5();
+        testOslusiadas();
+        System.out.println("OK");
+    }
+    
+    public void testSmallKey5() {
+        fr = new FileResource("messages/titus-small_key5.txt");
+        CaesarCracker cc = new CaesarCracker();
+        
+        String original = cc.decrypt(fr.asString());
+        
+        System.out.println(original);
+    }
+    
+    public void testOslusiadas() {
+        fr = new FileResource("messages/oslusiadas_key17.txt");
+        CaesarCracker cc = new CaesarCracker('a');
+        
+        String original = cc.decrypt(fr.asString());
+        
+        System.out.println(original);
+    }
 }
--- java/strings/VigenereProgram/VigenereCipherTester.java
+++ java/strings/VigenereProgram/VigenereCipherTester.java
@@ -5,6 +5,29 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class VigenereCipherTester {
 
+import edu.duke.FileResource;
+
+public class VigenereCipherTester {
+    private FileResource fr = new FileResource("messages/titus-small.txt");
+    private String encrypted = "";
+    private VigenereCipher vc = new VigenereCipher(new int[]{17, 14, 12, 4});
+    
+    public void performTests() {
+        testVigenereEncryption();
+        testVigenereDecryption();
+        System.out.println("OK");
+    }
+    
+    public void testVigenereEncryption() {
+        System.out.println("Before encryption: " + fr.asString());
+        System.out.println("Key used: " + vc.toString());
+        encrypted = vc.encrypt(fr.asString());
+        System.out.println("Encrypted: " + encrypted);
+    }
+    
+    public void testVigenereDecryption() {
+        String original = vc.decrypt(encrypted);
+        System.out.println("Original: " + original);
+    }
 }
